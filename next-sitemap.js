b7cb1f7a6b60275525a0638121c8205281b393b0
Amaterasu <caletina.marklester@gmail.com>
2026-01-03 15:15:04 +0800

    updating coffee shop

--- next-sitemap.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://kapehan.ph',
-  generateRobotsTxt: true, // (optional) generate robots.txt
-  changefreq: 'daily',
-  priority: 0.7,
-}
